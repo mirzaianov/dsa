6f5a6de2e81fa43f264be2921d0cb48ba2d6f801
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2025-10-25 10:46:00 +0300

    feat: 34

---
+++ 100js/34.removeFirstAndLast.js
@@ -0,0 +1,13 @@
+function removeFirstAndLast(source, target) {
+  return source
+    .replace(target, '')
+    .split('')
+    .reverse()
+    .join('')
+    .replace(target.split('').reverse().join(''), '')
+    .split('')
+    .reverse()
+    .join('');
+}
+
+export default removeFirstAndLast;
