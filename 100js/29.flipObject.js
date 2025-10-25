13d2d29f2ae10fd30442d877b083d7935fb0f339
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2025-10-25 09:47:20 +0300

    feat: 29

---
+++ 100js/29.flipObject.js
@@ -0,0 +1,15 @@
+function flipObject(people) {
+  const res = {};
+
+  for (const item in people) {
+    if (res[people[item]] === undefined) {
+      res[people[item]] = [item];
+    } else {
+      res[people[item]] = [...res[people[item]], item];
+    }
+  }
+
+  return res;
+}
+
+export default flipObject;
