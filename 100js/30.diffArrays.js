f55acfad0dfe27da7cc398e2f5ede24dd7377666
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2025-10-25 10:45:44 +0300

    feat: 30

---
+++ 100js/30.diffArrays.js
@@ -0,0 +1,17 @@
+function diffArrays(numbers1, numbers2) {
+  const set1 = new Set(numbers1);
+  const set2 = new Set(numbers2);
+  const res = [];
+
+  for (const item1 of set1.values()) {
+    if (!set2.has(item1)) res.push(item1);
+  }
+
+  for (const item2 of set2.values()) {
+    if (!set1.has(item2)) res.push(item2);
+  }
+
+  return res;
+}
+
+export default diffArrays;
