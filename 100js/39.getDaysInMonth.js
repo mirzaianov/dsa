eca52391cb01d3537ed308a011af971d2bd524ca
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2025-10-26 13:04:50 +0300

    feat: 39

---
+++ 100js/39.getDaysInMonth.js
@@ -0,0 +1,13 @@
+function getDaysInMonth(date) {
+  const year = date.getFullYear();
+  const month = date.getMonth();
+
+  const currentMonthsFirstDay = new Date(year, month, 1);
+  const nextMonthsFirstDay = month === 11 ? new Date(year + 1, 0, 1) : new Date(year, month + 1, 1);
+
+  const millisecondsInOneDay = 24 * 60 * 60 * 1000;
+
+  return Math.floor((nextMonthsFirstDay - currentMonthsFirstDay) / millisecondsInOneDay);
+}
+
+export default getDaysInMonth;
