028d8be0a2516d8d905f34fb92a04b830f606ad6
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2025-10-27 19:25:54 +0300

    feat: 40

---
+++ 100js/40.formatDateTime.js
@@ -0,0 +1,14 @@
+function formatDateTime(date, locale) {
+  let options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: false,
+  };
+
+  return new Intl.DateTimeFormat(locale, options).format(date);
+}
+
+export default formatDateTime;
