11b786e51209892b757fc756fda506d737ede08a
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2025-10-29 18:52:05 +0300

    feat: 45

---
+++ 100js/45.rgbToHex.js
@@ -0,0 +1,15 @@
+function decToHex(color) {
+  return Number(color).toString(16).padStart(2, 0);
+}
+
+function rgbToHex(rgbColor) {
+  const [r, g, b] = rgbColor
+    .split('')
+    .slice(4, rgbColor.length - 1)
+    .join('')
+    .split(', ');
+
+  return `#${decToHex(r)}${decToHex(g)}${decToHex(b)}`;
+}
+
+export default rgbToHex;
