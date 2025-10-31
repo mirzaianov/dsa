5315a6b8eba586b5b4b4dcf81c7aa65712827ad9
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2025-10-31 18:37:30 +0300

    feat: 50

---
+++ 100js/50.mergeSortedArrays.js
@@ -0,0 +1,23 @@
+function mergeSortedArrays(numbers1, numbers2) {
+  const res = [];
+  let i = 0;
+  let j = numbers2.length - 1;
+
+  while (i < numbers1.length && j >= 0) {
+    if (numbers1[i] < numbers2[j]) {
+      res.push(numbers1[i]);
+      i += 1;
+    } else if (numbers1[i] > numbers2[j]) {
+      res.push(numbers2[j]);
+      j -= 1;
+    } else {
+      res.push(numbers1[i], numbers2[j]);
+      i += 1;
+      j -= 1;
+    }
+  }
+
+  return [...res, ...numbers1.slice(i), ...numbers2.slice(0, j + 1).reverse()];
+}
+
+export default mergeSortedArrays;
