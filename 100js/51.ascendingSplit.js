f218dcfc747215b2db34757a0c399293795aaf58
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2025-11-01 16:04:47 +0300

    feat: 51

---
+++ 100js/51.ascendingSplit.js
@@ -0,0 +1,23 @@
+function ascendingSplit(numbers) {
+  const n = numbers.length;
+
+  if (n < 2) return numbers;
+
+  const res = [];
+  let temp = [numbers[0]];
+
+  for (let i = 1; i < n; i += 1) {
+    if (numbers[i - 1] > numbers[i]) {
+      res.push([...temp]);
+      temp.length = 0;
+    }
+
+    temp.push(numbers[i]);
+
+    if (i === n - 1) res.push([...temp]);
+  }
+
+  return res;
+}
+
+export default ascendingSplit;
