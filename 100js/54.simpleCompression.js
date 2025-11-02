0cbbbce77482366b265559a910c297d7345083d8
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2025-11-02 17:36:22 +0300

    feat: 54

---
+++ 100js/54.simpleCompression.js
@@ -0,0 +1,26 @@
+function simpleCompression(text) {
+  const n = text.length;
+
+  if (n === 0) return '';
+
+  let res = '';
+  let i = 0;
+
+  while (i < n) {
+    let curr = text[i];
+    let count = 1;
+
+    while (curr === text[i + 1]) {
+      count += 1;
+      i += 1;
+    }
+
+    count === 1 ? (res += curr) : (res += `${curr}${count}`);
+
+    i += 1;
+  }
+
+  return res;
+}
+
+export default simpleCompression;
