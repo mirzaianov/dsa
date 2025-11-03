7f090f02c6c06d5cdda035a3e75dbb61239906a2
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2025-11-03 18:26:20 +0300

    feat: 55

---
+++ 100js/55.partitionArray.js
@@ -0,0 +1,12 @@
+function partitionArray(numbers, predicateCallback) {
+  const fail = [];
+  const pass = [];
+
+  for (const n of numbers) {
+    predicateCallback(n) ? pass.push(n) : fail.push(n);
+  }
+
+  return { fail, pass };
+}
+
+export default partitionArray;
