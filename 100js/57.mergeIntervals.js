78b2cf9af1f26a07680a4fe6110adcb88613f8a1
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2025-11-04 15:17:59 +0300

    feat: 57

---
+++ 100js/57.mergeIntervals.js
@@ -0,0 +1,30 @@
+function mergeIntervals(intervals, newIntervals) {
+  const sorted = [...intervals, ...newIntervals].sort((a, b) => a.from - b.from || a.to - b.to);
+  const n = sorted.length;
+
+  if (n < 2) return sorted;
+
+  const res = [];
+
+  for (let i = 1; i < n; i += 1) {
+    const curr = sorted[i];
+    const last = res.pop() || sorted[0];
+
+    if (last.to < curr.from) {
+      res.push(last, curr);
+    } else {
+      const interval = {
+        numbers: [...last.numbers, ...curr.numbers].sort((a, b) => a - b),
+        from: last.from,
+      };
+
+      last.to < curr.to ? (interval.to = curr.to) : (interval.to = last.to);
+
+      res.push(interval);
+    }
+  }
+
+  return res;
+}
+
+export default mergeIntervals;
